41b0014e3c2c4b5c4478050a35b075946a490c18
= <=>
2022-10-21 16:49:34 +0300

    Cancel all orders

--- kernel/common_spread_bot/spread_bot.php
+++ kernel/common_spread_bot/spread_bot.php
@@ -55,6 +55,7 @@ $bot_create_only = new Exmo($keys[0]['api_key'], $keys[0]['secret_key']);
 $bot = new Ccxt($exchange, $keys[1]['api_key'], $keys[1]['secret_key']);
 $bot_only_for_balances_and_open_orders = new Ccxt($exchange, $keys[2]['api_key'], $keys[2]['secret_key']);
 
+$bot_only_for_balances_and_open_orders->cancelAllOrder();
 $balances = $bot_only_for_balances_and_open_orders->getBalances($assets);
 
 $real_orders = [];
